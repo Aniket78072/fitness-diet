0e2cd8051e2a70cf943cb540fb33884a4b251169
aniket <aniketpathania68393@gamil.com>
2025-09-07 17:44:55 +0530

    Fix AI functionality and security issues

--- fitness-app/vite.config.js
+++ fitness-app/vite.config.js
@@ -4,5 +4,12 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react() ,tailwindcss()],
+  plugins: [react(), tailwindcss()],
+  esbuild: {
+    loader: 'jsx',
+    include: /\.(jsx|js)$/,
+  },
+  optimizeDeps: {
+    include: ['axios'],
+  },
 })
